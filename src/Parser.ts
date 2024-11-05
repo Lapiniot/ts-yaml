12f3240bdf5325bc173621138c4e3c09e430cbd8
Lapiniot <aossss@gmail.com>
2024-11-05 15:27:41 +0200

    Provide minimal viable draft implementation for YAML parser #1

---
+++ src/Parser.ts
@@ -0,0 +1,149 @@
+import { Token, Tokenizer, TokenKind } from "./Tokenizer";
+
+export default class {
+    private constructor() { }
+
+    static parse(text: string): any {
+        const context = new Context();
+        for (const token of new Tokenizer(text)) {
+            context.handle(token);
+        }
+
+        return context.node;
+    }
+}
+
+class Context {
+    private state!: State;
+    readonly entries: Array<any>;
+    readonly enterState: State;
+
+    constructor() {
+        this.entries = [];
+        this.transitionTo(this.enterState = new ConstructMappingState({}, 'root', -1));
+    }
+
+    get node() {
+        return this.enterState.node;
+    }
+
+    transitionTo(state: State<unknown>) {
+        this.state = state;
+        this.state.setContext(this);
+    }
+
+    handle(token: Token) {
+        this.state.handle(token);
+    }
+}
+
+abstract class State<T = unknown> {
+    context!: Context;
+
+    constructor(public readonly node: T) {
+    }
+
+    setContext(context: Context) {
+        this.context = context;
+    }
+
+    abstract handle(token: Token): void;
+}
+
+class ConstructMappingState extends State<Record<string, any>> {
+    constructor(node: Record<string, any>, private key: string, private indent: number) {
+        super(node);
+    }
+
+    handle(token: Token): void {
+        switch (token.kind) {
+            case TokenKind.Scalar:
+                if (token.indent > this.indent) {
+                    const current = this.node[this.key];
+                    if (current) {
+                        this.node[this.key] = current + " " + token.value;
+                    } else {
+                        this.node[this.key] = token.value;
+                    }
+                } else {
+                    // throw
+                }
+                break;
+            case TokenKind.MappingKey:
+                {
+                    const { indent, key, column: col, line: ln } = token;
+                    if (indent > this.indent) {
+                        if (this.node[this.key]) {
+                            throw new Error(`Mapping key is not expected at this position (Ln ${ln}, Col ${col})`);
+                        }
+
+                        this.context.transitionTo(new ConstructMappingState(this.context.entries[indent] = this.node[this.key] = { [key]: null }, key, indent));
+                    } else {
+                        const node = this.context.entries[indent];
+                        if (node) {
+                            node[key] = null;
+                            this.context.transitionTo(new ConstructMappingState(node, key, indent));
+                        }
+                    }
+                }; break;
+            case TokenKind.SequenceEntry:
+                {
+                    const { indent } = token;
+                    if (indent > this.indent) {
+                        this.context.transitionTo(new ConstructSequenceState(this.context.entries[indent] = this.node[this.key] = [], indent));
+                    } else {
+                        const node = this.context.entries[indent];
+                        if (node) {
+                            this.context.transitionTo(new ConstructSequenceState(node, indent));
+                        }
+                    }
+                }; break;
+            case TokenKind.Comment: break;
+        }
+    }
+}
+
+class ConstructSequenceState extends State<Array<any>> {
+    constructor(node: Array<any>, private indent: number) {
+        super(node);
+    }
+
+    handle(token: Token): void {
+        switch (token.kind) {
+            case TokenKind.Scalar:
+                this.node.push(token.value);
+                break;
+            case TokenKind.MappingKey:
+                {
+                    const { indent, key } = token;
+                    if (indent > this.indent) {
+                        const value = { [key]: null };
+                        this.node.push(value);
+                        this.context.entries[indent] = value;
+                        this.context.transitionTo(new ConstructMappingState(value, key, indent));
+                    } else {
+                        const node = this.context.entries[indent];
+                        if (node) {
+                            node[key] = null;
+                            this.context.transitionTo(new ConstructMappingState(node, key, indent));
+                        } else {
+                            throw new Error(`Mapping should not start at this position (Ln ${token.line}, Col ${token.column})`);
+                        }
+                    }
+                }; break;
+            case TokenKind.SequenceEntry:
+                {
+                    const { indent } = token;
+                    if (indent > this.indent) {
+                        throw new Error(`Sequence entry is not expected at this position (Ln ${token.line}, Col ${token.column})`);
+                    } else {
+                        const node = this.context.entries[indent];
+                        if (node) {
+                            this.context.transitionTo(new ConstructSequenceState(node, indent));
+                        }
+                    }
+                }; break;
+            case TokenKind.Comment: break;
+        }
+    }
+}
---
+++ src/Tokenizer.ts
@@ -0,0 +1,250 @@
+export class Tokenizer implements Iterable<Token> {
+    constructor(private readonly text: string) {
+    }
+
+    [Symbol.iterator](): Iterator<Token, any, any> {
+        return new TokenizerCore(this.text);
+    }
+}
+
+export enum TokenKind {
+    Scalar,
+    MappingKey,
+    SequenceEntry,
+    Comment
+}
+
+export type Token = Location & (
+    { kind: TokenKind.Comment, text: string } |
+    { kind: TokenKind.Scalar, value: string | null, indent: number } |
+    { kind: TokenKind.MappingKey, key: string, indent: number } |
+    { kind: TokenKind.SequenceEntry, indent: number }
+)
+
+type Location = { line: number, column: number }
+
+enum Indicators {
+    DoubleQuote = 0x22,
+    SingleQuote = 0x27,
+    SequenceEntry = 0x2D,
+    MappingKey = 0x3F,
+    MappingValue = 0x3A,
+    Comment = 0x23,
+    CR = 0x0D,
+    LF = 0x0A
+}
+
+class TokenizerCore implements Iterator<Token> {
+    text: string;
+    private state!: State;
+
+    constructor(text: string) {
+        this.text = text;
+        this.transitionTo(new ScalarState(0, undefined, 1, 1));
+    }
+
+    next(): IteratorResult<Token, undefined> {
+        return this.state.next();
+    }
+
+    transitionTo(state: State) {
+        this.state = state;
+        this.state.setContext(this);
+    }
+}
+
+abstract class State {
+    protected context!: TokenizerCore;
+
+    constructor(protected readonly start: number, protected readonly line: number, protected readonly column: number) { }
+
+    setContext(context: TokenizerCore) {
+        this.context = context;
+    }
+
+    protected moveNext(state: State): IteratorResult<Token, undefined> {
+        this.context.transitionTo(state);
+        return this.context.next();
+    }
+
+    abstract next(): IteratorResult<Token, undefined>;
+}
+
+class ScalarState extends State {
+    constructor(readonly start: number, private readonly indent: number | undefined, line: number, column: number) {
+        super(start, line, column);
+    }
+
+    next(): IteratorResult<Token, undefined> {
+        const chunks: Array<string> = [];
+        const { context: { text, text: { length } } } = this;
+
+        for (let { start: index, start, indent = 0, indent: blockIndent, line, column } = this;
+            index < length;
+            index++, indent = 0, start = index, line++, column = 1) {
+
+            for (; index < length && isWhiteSpace(text.codePointAt(index)); index++);
+
+            if (index >= length) {
+                break;
+            }
+
+            // compute effective line indentation
+            const padding = index - start;
+            indent += padding;
+            // capture first line indentation as effective block indentation level if it was not provided explicitely
+            // (this.indent property)
+            blockIndent = blockIndent || indent;
+
+            const code = text.codePointAt(index);
+
+            // Less indented block detection
+            if (indent < blockIndent && !isEOL(code)) {
+                this.context.transitionTo(new ScalarState(start, undefined, line, 1));
+                return {
+                    value: {
+                        kind: TokenKind.Scalar, value: fold(chunks),
+                        indent: blockIndent, line: this.line, column: this.column
+                    }
+                };
+            }
+
+            // Sequence entry marker detection
+            if (code === Indicators.SequenceEntry) {
+                if (index + 1 >= length || isWhiteSpaceOrEOL(text.codePointAt(index + 1))) {
+                    if (chunks.length === 0) {
+                        this.context.transitionTo(new ScalarState(index + 1, indent + 1, line, column + padding + 1));
+                        return { value: { kind: TokenKind.SequenceEntry, indent, line, column: column + padding } };
+                    }
+
+                    this.context.transitionTo(new ScalarState(start, undefined, line, 1));
+                    return {
+                        value: {
+                            kind: TokenKind.Scalar, value: fold(chunks),
+                            indent: blockIndent, line: this.line, column: this.column
+                        }
+                    };
+                }
+
+                index++;
+            }
+
+            scan_line_loop:
+            for (; index < length; index++) {
+                switch (text.codePointAt(index)) {
+                    case Indicators.LF: {
+                        break scan_line_loop;
+                    }
+
+                    // Mapping key-value pair detection
+                    case Indicators.MappingValue: {
+                        if (index + 1 < length && !isWhiteSpaceOrEOL(text.codePointAt(index + 1))) {
+                            break;
+                        }
+
+                        if (chunks.length === 0) {
+                            this.context.transitionTo(new ScalarState(index + 1, indent + 1, line, column + index - start + 1));
+                            return {
+                                value: {
+                                    kind: TokenKind.MappingKey, key: text.substring(start, index).trim(),
+                                    indent, line, column: column + padding
+                                }
+                            };
+                        }
+
+                        this.context.transitionTo(new ScalarState(start, undefined, line, 1));
+                        return {
+                            value: {
+                                kind: TokenKind.Scalar, value: fold(chunks),
+                                indent: blockIndent, line: this.line, column: this.column
+                            }
+                        };
+                    }
+
+                    // Comment line detection
+                    case Indicators.Comment: {
+                        if (index - 1 > 0 && !isWhiteSpaceOrEOL(text.codePointAt(index - 1))) {
+                            break;
+                        }
+
+                        this.context.transitionTo(new CommentState(index + 1, line, column + padding));
+
+                        if (chunks.length === 0) {
+                            return this.context.next();
+                        }
+
+                        chunks.push(text.substring(start, index).trim());
+                        this.context.transitionTo(new CommentState(index + 1, line, column + padding));
+                        return {
+                            value: {
+                                kind: TokenKind.Scalar, value: fold(chunks),
+                                indent: blockIndent, line: this.line, column: this.column
+                            }
+                        }
+                    }
+                }
+            }
+
+            chunks.push(text.substring(start, index).trim());
+        }
+
+        this.context.transitionTo(new FinalState());
+
+        const value = fold(chunks);
+        return value
+            ? {
+                value: {
+                    kind: TokenKind.Scalar, value,
+                    indent: this.indent || 0,
+                    line: this.line, column: this.column
+                }
+            }
+            : this.context.next()
+    }
+}
+
+class CommentState extends State {
+    next(): IteratorResult<Token, undefined> {
+        const { start, context: { text, text: { length } } } = this;
+        let index = start;
+
+        for (; index < length; index++) {
+            if (text.codePointAt(index) === Indicators.LF)
+                break;
+        }
+
+        this.context.transitionTo(new ScalarState(index + 1, undefined, this.line + 1, 1));
+        return {
+            value: {
+                kind: TokenKind.Comment, text: text.substring(start, index).trim(),
+                line: this.line, column: this.column
+            }
+        }
+    }
+}
+
+class FinalState extends State {
+    constructor() {
+        super(0, 0, 0);
+    }
+
+    next(): IteratorResult<Token, undefined> {
+        return { done: true, value: undefined };
+    }
+}
+
+function fold(chunks: Array<string>) {
+    return chunks.reduce((prev, curent) => prev + (curent ? (prev[prev.length - 1] !== "\n" ? " " : "") + curent : "\n"), "").trim() || null;
+}
+
+function isWhiteSpace(code: number | undefined) {
+    return code === 0x20 || code === 0x09;
+}
+
+function isEOL(code: number | undefined) {
+    return code === Indicators.LF || code === Indicators.CR;
+}
+
+function isWhiteSpaceOrEOL(code: number | undefined) {
+    return code === 0x20 || code === 0x09 || code === Indicators.LF || code === Indicators.CR;
+}
